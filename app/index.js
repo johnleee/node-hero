4410cfe1719ff1c98fcd75a2885f30abda81dc25
John Lee <jlee@openlane.com>
2017-03-31 13:08:43 -0700

    Node.js Authentication using Passport.js

--- app/index.js
+++ app/index.js
@@ -1,12 +1,17 @@
 // app/index.js
-const calc = require('./calc')
+const express = require('express')  
+const passport = require('passport')  
+const session = require('express-session')  
+const RedisStore = require('connect-redis')(session)
 
-const numbersToAdd = [  
-  3,
-  4,
-  10,
-  2
-]
-
-const result = calc.sum(numbersToAdd)  
-console.log(`The result is: ${result}`)  
+const app = express()  
+app.use(session({  
+  store: new RedisStore({
+    url: config.redisStore.url
+  }),
+  secret: config.redisStore.secret,
+  resave: false,
+  saveUninitialized: false
+}))
+app.use(passport.initialize())  
+app.use(passport.session()) 
